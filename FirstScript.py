f96249ef62faf37cb65afe0bbfd7836ef1847d93
Soreasan <soreasan@gmail.com>
2017-01-13 14:29:18 -0700

    Followed an example from Learning Python

---
+++ FirstScript.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+# A first python script 
+# Taken from "Learning Python" 5th edition pg. 55.
+import sys      # Load a library module
+print(sys.platform)
+print(2 ** 100) # Raise 2 to a power
+x = 'Spam!'
+print(x * 8)
