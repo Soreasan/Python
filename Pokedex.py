64ebbae2861144a6129275165cf93b3158daacd4
Kenneth Adair <SOREASAN@GMAIL.COM>
2017-02-06 16:25:31 -0700

    Created Pokedex.py which is the PokemonRetriever.py but with a Graphical User Interface.

---
+++ Pokedex.py
@@ -0,0 +1,35 @@
+#!/usr/bin/python3
+# Kenneth Adair
+# This script retrieves a pokemon's name based on the number the user inputs.
+# Based on a Twitter retrieving script from Pluralsight's 
+# "Raspberry Pi for Developers" course by Jon Flanders
+# However my version uses a completely different api.
+# The concepts are the same though
+# This version tacks a very basic GUI onto the program
+
+import tkinter
+import http.client
+import json
+
+root = tkinter.Tk()
+listbox = tkinter.Listbox(root)
+text = tkinter.Entry(root)
+
+def buttonclick():
+    c = http.client.HTTPConnection('pokeapi.co')
+    c.request('GET','/api/v2/pokemon/{0}/'.format(text.get()))
+    r = c.getresponse()
+    data = r.read()
+    datas = str(data, 'utf-8')
+    o = json.loads(datas)
+    pokemon = o['name']
+    listbox.delete(0, tkinter.END)
+    listbox.insert(tkinter.END, pokemon)
+    #listbox.insert(tkinter.END, 'test')
+    
+button = tkinter.Button(root, text="Enter a number \nbetween 1 and 811:", command=buttonclick)
+button.pack(side="left")
+text.pack(side="left")
+listbox.pack(side="bottom", fill="both", expand=True)
+
+root.mainloop()
